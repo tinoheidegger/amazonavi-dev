dd3f6832f9fbb343b1b94e38e049a227266d4b56
tinoheidegger <tinoheidegger83@gmail.com>
2021-09-23 03:43:07 +0000

    Create Register Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import CartScreen from "./screen/CartScreen";
 import { useDispatch, useSelector } from "react-redux";
 import SigninScreen from "./screen/SigninScreen";
 import { signout } from "./actions/userActions";
+import RegisterScreen from "./screen/RegisterScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -56,6 +57,7 @@ function App() {
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
           <Route path="/signin" component={SigninScreen} exact></Route>
+          <Route path="/register" component={RegisterScreen} exact></Route>
         </main>
         <footer className="row center">All rights reserved. Amazonavi</footer>
       </div>
